1978b3d573660cfb763e662e1ce82841e3a5f37b
VeshaguriG <veshaguriwot@gmail.com>
2021-11-02 21:17:27 +0400

    js

---
+++ script/script.js
@@ -0,0 +1,130 @@
+// //var a = 5;
+// //var b = "12"
+// //var c = a + 14 + b;
+
+const { equal } = require("assert")
+const { copyFileSync } = require("fs")
+
+// //console.log(c);
+
+// // var a = "string"
+
+// // console.log(typeof a);
+// var n = g; 
+// var a = n.toString( 10)
+// var b = n.toString( 8)
+// var c = n.toString( 16)
+// var d = n.toString( 2)
+// console.log(b)
+// console.log(a)
+// console.log(c)
+// console.log(d)
+
+// == ======
+// let a = 3
+
+// if ( a < 4)
+// {
+//     console.log('equal 5')}
+//     else if (a === 4)
+//     {console.log('not equals')
+// }
+
+
+// let x = 5
+
+// switch (new Date().getMonth()) { 
+//     case 1: 
+//     console.log('sddd');
+//     break;
+//     case 2: 
+//     console.log('3');
+//     break;
+//     case 3: 
+//     console.log('3');
+//     break;
+//     case 4: 
+//     console.log('3');
+//     break;
+//     case 5: 
+//     console.log('3');
+//     break;
+//     case 6: 
+//     console.log('3');
+//     break;
+//     case 7: 
+//     console.log('3');
+//     break;
+//     case 10: 
+//     console.log('3');
+//     break;
+// }
+
+// if ( x === 6) {
+//     console.log('123')
+
+
+// }
+// else { console.log('dss')}
+// let x = 5;
+
+// x === 6 ? console.log('sss') : x === 7 ? console.log ('dss') : console.log('22')
+
+// let x = "6";
+// let y = "8";
+// let q = "2";
+
+ if ( x > y && x > q ) {
+     console.log("largestNmb x")
+ }else if ( y > x && y > q) { 
+     console.log("largestNmb y")
+ } else if ( q > x && q > y) {
+     console.log("largestNmb q")
+ }
+
+let std1 = "70";
+let std2 = "50";
+let std3 = "94";
+let std4 = "65";
+
+if ( std1 < 60) {  console.log('F')}
+ else if ( std1 < 70 ) { 
+    console.log('D')
+}  else if ( std1 < 80 ) { 
+    console.log('C')
+}  else if ( std1 < 90 ) { 
+    console.log('B')
+} else if ( std1 < 100 ) { 
+    console.log('A')
+} 
+
+if ( std2 < 60) {  console.log('F')}
+ else if ( std2 < 70 ) { 
+    console.log('D')
+}  else if ( std2 < 80 ) { 
+    console.log('C')
+}  else if ( std < 90 ) { 
+    console.log('B')
+} else if ( std2 < 100 ) { 
+    console.log('A')
+} 
+if ( std3 < 60) {  console.log('F')}
+ else if ( std3 < 70 ) { 
+    console.log('D')
+}  else if ( std3 < 80 ) { 
+    console.log('C')
+}  else if ( std3 < 90 ) { 
+    console.log('B')
+} else if ( std3 < 100 ) { 
+    console.log('A')
+} 
+if ( std4 < 60) {  console.log('F')}
+ else if ( std4 < 70 ) { 
+    console.log('D')
+}  else if ( std4 < 80 ) { 
+    console.log('C')
+}  else if ( std4 < 90 ) { 
+    console.log('B')
+} else if ( std4 < 100 ) { 
+    console.log('A')
+} 
